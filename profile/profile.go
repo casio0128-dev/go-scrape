af849b1617dd15e96633f85929ff2aa6f517d601
casio0128-dev <sweet.cafe0141@gmail.com>
2022-04-08 19:37:07 +0900

    日付置換機能の実装

--- profile/profile.go
+++ profile/profile.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"regexp"
 	"strings"
+	"time"
 )
 
 type Profiles []Profile
@@ -35,14 +36,79 @@ func Parse(str string, vars Variable) (string, error) {
 	return result, nil
 }
 
-// TODO: Parseと同様の処理を一部Actionに追加する
-func parseDate() {
-	dateReg, _ := regexp.Compile(`{(Y{4})}|{(M{1,2})}|{(D{1,2})}|{(W{1,2})}`)
+func parseDate(str string) (string, error) {
+	result := str
+	dateReg, err := regexp.Compile(`{(Y{4})}|{(M{1,2})}|{(D{1,2})}|{(W{1,4})}`)
+	if err != nil {
+		return "", err
+	}
 
-	fmt.Println(dateReg.FindAllStringSubmatch(`{YYYY}/{MM}/{DD}`, -1))
-	fmt.Println(dateReg.FindAllStringSubmatch(`{YYYY}/{M}/{D}`, -1))
-	fmt.Println(dateReg.FindAllStringSubmatch(`{YYYY}/{M}/{DD}`, -1))
-	fmt.Println(dateReg.FindAllStringSubmatch(`{YYYY}/{MM}/{D}`, -1))
-	fmt.Println(dateReg.FindAllStringSubmatch(`{YYYY}{MM}{DD}{WW}`, -1))
-	fmt.Println(dateReg.FindAllStringSubmatch(`{YYYY}-{M}-{D}({W})`, -1))
+	for _, match := range dateReg.FindAllStringSubmatch(str, -1) {
+		symbol := match[1]
+		oldStr := fmt.Sprintf("{%s}", symbol)
+		newStr := fmt.Sprintf("%s", parseDateFormat(symbol))
+		result = strings.ReplaceAll(result, oldStr, newStr)
+	}
+	return result, nil
+}
+
+var (
+	_YYYY = []string{"YYYY", "2006"}
+	_MM   = []string{"MM", "01"}
+	_M    = []string{"M", "1"}
+	_DD   = []string{"DD", "02"}
+	_D    = []string{"D", "2"}
+	_WWWW = []string{"WWWW", "3"}
+	_WWW  = []string{"WWW", "2"}
+	_WW   = []string{"WW", "1"}
+	_W    = []string{"W", "0"}
+)
+
+func parseWeekday(weekday time.Weekday) []string {
+	switch weekday {
+	case time.Sunday:
+		return []string{"日", "Su", "Sun", "Sunday"}
+	case time.Monday:
+		return []string{"月", "Mo", "Mon", "Monday"}
+	case time.Tuesday:
+		return []string{"火", "ue", "Tue", "Tuesday"}
+	case time.Wednesday:
+		return []string{"水", "ne", "Wed", "Wednesday"}
+	case time.Thursday:
+		return []string{"木", "ur", "Thu", "Thursday"}
+	case time.Friday:
+		return []string{"金", "Fr", "Fri", "Friday"}
+	case time.Saturday:
+		return []string{"土", "tu", "Sat", "Saturday"}
+	}
+}
+
+func parseDateFormat(pattern string) string {
+	now := time.Now()
+
+	switch pattern {
+	case _YYYY[0]:
+		return now.Format(_YYYY[1])
+	case _MM[0]:
+		return now.Format(_MM[1])
+	case _M[0]:
+		return now.Format(_M[1])
+	case _DD[0]:
+		return now.Format(_DD[1])
+	case _D[0]:
+		return now.Format(_D[1])
+	}
+
+	week := parseWeekday(now.Weekday())
+	switch pattern {
+	case _WWWW[0]:
+		return week[3]
+	case _WWW[0]:
+		return week[2]
+	case _WW[0]:
+		return week[1]
+	case _W[0]:
+		return week[0]
+	}
+	return ""
 }
